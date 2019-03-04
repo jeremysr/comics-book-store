1c4012ba7ac83a3428960c32f1a2bc4573ff82da
jeremysr <47540315+jeremysr@users.noreply.github.com>
2019-03-05 10:31:15 +1300

    Add files via upload

--- comic-books.py
+++ comic-books.py
@@ -7,11 +7,38 @@ class Comic:
     
     _ids = count(0)
     
-    def __init__(self, title, stock, desc):
+    def __init__(self, title, stock, sold):
         self.id = next(self._ids)
         self.title = title
         self.stock = stock
-        self.desc = desc
+        self.sold = sold
+        
+        
+comics = [
+    Comic("Water Woman","3","0"),
+    Comic("Lizard Man","12","0"),
+    Comic("Super Dude","8","0"),
+]
+
+
+#pages
+#index page
+@route('/')
+@view('index')
+def index():
+    pass
+
+#stock-levels page
+@route('/stock-levels')
+@view('stock-levels')
+def stock_levels():
+    data = dict (stock_list = comics)
+    return data
+
+
+run(host='0.0.0.0', port = 8080, reloader = True, debug = True)
+
+        
         
         
 
